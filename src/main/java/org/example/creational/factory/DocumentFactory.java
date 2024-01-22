6262eaf6e311fa4f735ef60817e8133586f99c57
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-22 14:07:40 +0300

    factory pattern

--- src/main/java/org/example/creational/factory/DocumentFactory.java
+++ src/main/java/org/example/creational/factory/DocumentFactory.java
@@ -1,7 +1,20 @@
 package org.example.creational.factory;
 
 public class DocumentFactory {
-    public static Document getDocument(String type){
-        return null;
+    public static Document getDocument(DocumentType type){
+        if(type==null)
+            throw new IllegalArgumentException("");
+
+        switch(type)
+        {
+            case PDF:
+                return new PDF();
+            case WORD:
+                return new Word();
+            case EXCEL:
+                return new Excel();
+            default:
+                throw new IllegalArgumentException("");
+        }
     }
 }
--- src/main/java/org/example/creational/factory/Main.java
+++ src/main/java/org/example/creational/factory/Main.java
@@ -2,6 +2,8 @@ package org.example.creational.factory;
 
 public class Main {
     public static void main(String[] args) {
-        PDF pdf = new PDF();
+        PDF pdf = (PDF) DocumentFactory.getDocument(DocumentType.PDF);
+
+        Excel excel = (Excel) DocumentFactory.getDocument(DocumentType.EXCEL);
     }
 }
