4b4a7db03e6aa88a9da9b2278d59023e9b50e93f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-22 14:33:03 +0300

    .

--- src/main/java/org/example/creational/abstractfactory/services/ProductServiceImpl.java
+++ src/main/java/org/example/creational/abstractfactory/services/ProductServiceImpl.java
@@ -1,7 +1,19 @@
 package org.example.creational.abstractfactory.services;
 
 
+import org.example.creational.abstractfactory.caching.CachingBase;
+import org.example.creational.abstractfactory.factories.CrossCuttingConcernFactory;
+import org.example.creational.abstractfactory.logging.LoggerBase;
+
 public class ProductServiceImpl implements ProductService{
+
+    private final CachingBase cachingBase;
+    private final LoggerBase loggerBase;
+
+    public ProductServiceImpl(CrossCuttingConcernFactory factory) {
+        this.cachingBase = factory.getCaching();
+        this.loggerBase = factory.getLogger();
+    }
 
     @Override
     public void add() {
