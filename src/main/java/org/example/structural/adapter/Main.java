99c2d386b8243e8bb5249fe4bcc90b8b5c80a239
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-23 10:36:15 +0300

    .

--- src/main/java/org/example/structural/adapter/Main.java
+++ src/main/java/org/example/structural/adapter/Main.java
@@ -1,7 +1,12 @@
 package org.example.structural.adapter;
 
+
+import org.example.structural.adapter.caching.InMemoryCaching;
+import org.example.structural.adapter.caching.adapters.RedisCacheAdapter;
+import org.example.structural.adapter.services.ProductServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-
+        ProductServiceImpl productService = new ProductServiceImpl(new RedisCacheAdapter());
     }
 }
